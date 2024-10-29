103bafe54c224735811c729cb1b954f62bf5acaa
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2024-10-29 17:27:28 -0500

    Change requests by Socrata

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
+This assestment if to ingest Fire incidents from San Francisco, downloaded
+from the page data.sfgov.org, in particular the dataset wr8u-xric, and ingested
+into a SQLite database (could be swiched to any other one in easy way).
 
+The develop environment was Debian, with an environment manage pyenv and poetry.
+The application run inside a docker, and output the SQLite database in the
+data directory under the name fire_incidents.db.
+
+The information about the data schema is in the attached file
+FIR-0001_DataDictionary_fire-incidents.xlsx.
+
+The LIMIT variable inside the scrap script is the limit records possible get
+as free connection.
 
 To build the container run:
 
@@ -9,5 +21,10 @@ docker build -t fire_incidents .
 After build the container, run with:
 
 ```sh
-docker run fire_incidents
+docker run -v ./data:/usr/src/app/data fire_incidents 
 ```
+
+
+And after running you can see the message:
+
+Data saved to SQL database.
--- src/scrapper.py
+++ src/scrapper.py
@@ -1,35 +1,73 @@
 import os
-import requests
 import pandas as pd
+from sodapy import Socrata
 from sqlalchemy import create_engine
 
-# URL to the data
-url = "https://data.sfgov.org/resource/wr8u-xric.csv"
 
-# Directory to save the database
-output_dir = os.environ.get("OUTPUT_DIR", "/usr/src/app/data")
+# Limit of data to be loaded
+LIMIT = 2000
+url = "data.sfgov.org"
+resource = "wr8u-xric"
+OUTPUT_DIR = "/usr/src/app/data"
+DB_FILE_NAME = "fire_incidents.db"
+DB_NAME = 'fire_incidents'
 
-# Ensure directory exists
-os.makedirs(output_dir, exist_ok=True)
 
-# Send a GET request to fetch the data
-response = requests.get(url)
+def main(output_dir):
+    # Unauthenticated client only works with public data sets. Note 'None'
+    # in place of application token, and no username or password:
+    client = Socrata(url, None)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Save data to a dataframe
-    data = pd.read_csv(url, parse_dates=['incident_date', 'alarm_dttm',
-                                         'arrival_dttm', 'close_dttm'])
-    
-    # Print the first few rows of the dataframe
-    print(data.head())
-    
-    # Connection string pointing to the output directory
-    database_file_path = os.path.join(output_dir, "fire_incidents.db")
-    engine = create_engine(f'sqlite:///{database_file_path}')
+    if client:
+        # Save data to a dataframe
+        results = client.get(resource, limit=LIMIT)
+        data = pd.DataFrame.from_records(results)
 
-    # Save the dataframe to an SQL table named 'fire_incidents'
-    data.to_sql('fire_incidents', con=engine, if_exists='replace', index=False)
-    print("Data saved to SQL database.")
-else:
-    print(f"Failed to fetch data. Status code: {response.status_code}")
+        # Connection string pointing to the output directory
+        database_file_path = os.path.join(output_dir, DB_FILE_NAME)
+        engine = create_engine(f'sqlite:///{database_file_path}')
+
+        data = add_lat_long(data)
+        data = fix_data_types(data)
+
+        # Save the dataframe to an SQL table named 'fire_incidents'
+        data.to_sql(DB_NAME, con=engine, if_exists='replace', index=False)
+        print("Data saved to SQL database.")
+    else:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+
+def output_dir():
+    # Directory to save the database
+    output_dir = os.environ.get("OUTPUT_DIR", OUTPUT_DIR)
+
+    # Ensure directory exists
+    os.makedirs(output_dir, exist_ok=True)
+
+    return output_dir
+
+def add_lat_long(data):
+    data['latitude'] = data['point'].apply(lambda x: x['coordinates'][0] if isinstance(x, dict) else None)
+    data['longitude'] = data['point'].apply(lambda x: x['coordinates'][1] if isinstance(x, dict) else None)
+
+    return data.drop(columns=['point'])
+
+def fix_data_types(data):
+    datetime_columns = ['incident_date', 'alarm_dttm', 'arrival_dttm', 'close_dttm']
+    number_columns = ['incident_number', 'suppression_personnel', 'ems_units',
+                      'ems_personnel', 'other_units', 'other_personnel',
+                      'estimated_property_loss', 'estimated_contents_loss',
+                      'fire_fatalities', 'fire_injuries', 'civilian_fatalities',
+                      'civilian_injuries', 'number_of_alarms',
+                      'floor_of_fire_origin', 'number_of_sprinkler_heads_operating']
+
+    for col in datetime_columns:
+        data[col] = pd.to_datetime(data[col])
+
+    for col in number_columns:
+        data[col] = pd.to_numeric(data[col])
+
+    return data
+
+
+if __name__ == "__main__":
+    main(output_dir())
